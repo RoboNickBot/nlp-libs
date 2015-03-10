6eb43ca3bae8f46540624f43cbcb3b2ff5269056
RoboNickBot <nicklewchenko92@gmail.com>
2015-03-09 20:28:11 -0500

    3gram-parsing works

--- src/Language/Identify/Parse.hs
+++ src/Language/Identify/Parse.hs
@@ -1,5 +1,6 @@
 module Language.Identify.Parse (readCrData) where
 
+import Data.Char (isSpace, isAlpha, toLower)
 import qualified Data.Map as M
 import qualified Data.Text as T
 import Text.ParserCombinators.Parsec
@@ -12,12 +13,33 @@ readCrData fpath =
      let ngs = (fmap M.fromList . parse ngramFile "err") s
      return ngs
 
-plaintext :: GenParser Char st [(T.Text, Int)]
-plaintext = do result <- many word
-               eof
-               return result
-               
-word = undefined
+readPlainText :: T.Text -> NGrams
+readPlainText = ngcount . concat . fmap parseWord 
+                . smooth . T.unpack
+
+ngcount :: [T.Text] -> NGrams
+ngcount = foldr (\t m -> if M.member t m
+                            then M.adjust (+1) t m
+                            else M.insert t 1 m)
+                M.empty
+
+data NGramTok = Start | End | Letter Char
+
+parseWord :: [NGramTok] -> [T.Text]
+parseWord (a:b:c:ts) = 
+  (T.pack . fmap fromTok) [a,b,c] : parseWord (b:c:ts)
+parseWord _ = []
+        
+fromTok Start = '<'
+fromTok End = '>'
+fromTok (Letter c) = c
+
+toTok :: String -> [NGramTok]
+toTok word = [Start] ++ (fmap Letter word) ++ [End]
+
+smooth :: String -> [[NGramTok]]
+smooth = fmap toTok . words . fmap toLower 
+         . filter (\c -> isAlpha c || isSpace c)
 
 ngramFile :: GenParser Char st [(T.Text, Int)]
 ngramFile = do result <- many line
