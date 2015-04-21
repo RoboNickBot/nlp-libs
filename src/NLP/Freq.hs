32ab0cc498ee17ea5b8758afd09d0d1151e7ed23
RoboNickBot <nicklewchenko92@gmail.com>
2015-04-20 21:16:01 -0500

    added cosineX function
    This is like cosineM, but allows for many languages to be cosine'd
    simultaneously (meaning, each call to the fetchFrequency function passed
    in returns the frequency of the current trigram for every langyage being
    considered).
    
    This should cut down on the number of database queries required while
    still only requesting trigrams that are actually needed.

--- src/NLP/Freq.hs
+++ src/NLP/Freq.hs
@@ -69,7 +69,7 @@ compareM :: (F f, Functor m)
 compareM nT acc (k, aV) = fmap ((+ acc).(* aV)) (nT k)
 
 len :: F f => FreqList f -> Double
-len = sqrt . fromIntegral . foldr (\a s -> a^2 + s) 0 
+len = sqrt . fromIntegral . foldr (\a s -> a^ (2::Int) + s) 0 
       . fmap snd . M.toList . freqMap
 
 prettyprint :: F a => FreqList a -> [String]
@@ -78,3 +78,42 @@ prettyprint (FreqList m) =
   in fmap show (L.sortBy 
                   (\a b -> compare (snd b) (snd a)) 
                   vals)
+
+initMap ls = M.fromList . zip ls . repeat
+
+cosineX :: (F f, Monad m, Functor m, Applicative m)
+        => [String]
+        -> FreqList f
+        -> ( (f -> m (M.Map String Int))
+           , (m [(String, Int)])      )
+        -> m (M.Map String Double)
+cosineX langs freqList (fetchTrigFreq, fetchLangLens) =
+  let initAcc = (M.fromList . zip langs . repeat) 0
+      foldVals = (M.toList . freqMap) freqList
+      thislen = len freqList
+  in do acc <- fmap fromIntegral <$> foldM (dotStep fetchTrigFreq) 
+                                           initAcc 
+                                           foldVals
+        lens <- fetchLangLens
+        return (foldl (\m (l,v) -> 
+                         M.adjust (lenDiv thislen (fromIntegral v)) l m)
+                      acc
+                      lens)
+
+lenDiv :: Double -> Double -> Double -> Double
+lenDiv lenA lenB accum = accum / (lenA * lenB)
+
+dotStep :: (F f, Monad m)
+        => (f -> m (M.Map String Int)) -- fetch function 
+        -> (M.Map String Int) -- accumulated value
+        -> (f, Int) -- feature being examined
+        -> m (M.Map String Int)
+dotStep nT acc (f,i) = do comp <- nT f
+                          return (dotCalc i comp acc)
+
+dotCalc :: Int -- freq of feature in target text
+        -> M.Map String Int -- freqs in candidate langs
+        -> M.Map String Int -- accumulated dot for candidate langs
+        -> M.Map String Int
+dotCalc i a b = foldl r b (M.toList a)
+  where r m (l,v) = M.adjust (\x -> v * i + x) l m
